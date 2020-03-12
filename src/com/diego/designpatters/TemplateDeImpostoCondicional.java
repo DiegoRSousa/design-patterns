be5c413af2ef501c0ad9308685c06d4526c8a8f6
Diego <diegors90@gmail.com>
2020-03-11 21:50:25 -0300

    template method

---
+++ src/com/diego/designpatters/TemplateDeImpostoCondicional.java
@@ -0,0 +1,15 @@
+package com.diego.designpatters;
+
+public abstract class TemplateDeImpostoCondicional implements Imposto{
+
+	@Override
+	public double calcula(Orcamento orcamento) {
+		if(deveUsarMaximaTaxacao(orcamento))
+			return maximaTaxacao(orcamento);
+		return minimaTaxacao(orcamento);
+	}
+
+	protected abstract double minimaTaxacao(Orcamento orcamento);
+	protected abstract double maximaTaxacao(Orcamento orcamento);
+	protected abstract boolean deveUsarMaximaTaxacao(Orcamento orcamento);
+}
